d5194770a3fd1f153cd8f3fe1389e1d54a45c7e2
Voctor <voctorbidoof@gmail.com>
2020-08-08 13:37:53 -0700

    Genres code

--- cogs/spotify.py
+++ cogs/spotify.py
@@ -8,19 +8,20 @@ import random
 
 # Spotify
 
-"""
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
 
-scope = "user-library-read"
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-"""
+
 
 # Sending announcements to other servers based on the on_ready event in main
 class Spotify(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    user_tracks = {}
+    queue = []
 
     @commands.group(name='spotify', aliases=['sptfy'])
     async def spotify(self, ctx):
@@ -33,17 +34,52 @@ class Spotify(commands.Cog):
     @spotify.command()
     async def check(self, ctx, user: discord.Member=None):
         user = user or ctx.author
-        print(user.activities)
+
         for activity in user.activities:
             if isinstance(activity, discord.Spotify):
                 # Using the Spotify API from Discord's provided track id
                 # This is broken
-                # track = sp.track(f'spotify:track:{activity.track_id}')
-                # print(track)
+                sp_track = sp.track(activity.track_id)
+                sp_artist = sp.artist(sp_track["artists"][0]["id"])
 
-                await ctx.send(f"{user} is listening to **{activity.title}** by {activity.artist}.")
+                await ctx.send(f"{user} is listening to **{activity.title}** by {activity.artist}.\nGenres: {', '.join(sp_artist['genres'])}")
                 return
         await ctx.send(f'{user} is not listening to a song.')
+    
+
+    # TODO: Figure out how to not fire more than once
+    # Check if user's Spotify changed
+
+    """
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        print("test")
+
+        old_track = '' 
+        new_track = ''
+
+        if before.id not in Spotify.user_tracks:
+            Spotify.user_tracks[str(before.id)] = ''
+
+        # Compare before
+        for activity in before.activities:
+            if isinstance(activity, discord.Spotify):
+                old_track = activity.track_id
+
+        # Compare after
+        for activity in after.activities:
+            if isinstance(activity, discord.Spotify):
+                new_track = activity.track_id
+
+        # print(f"{old_track != new_track}, {new_track != ''}, {new_track != user_tracks[str(before.id)]}")
+
+        # If the user changed Spotify tracks
+        if old_track != new_track and new_track != '' and new_track != Spotify.user_tracks[str(after.id)]:
+            # before in this context is used as Discord ID
+            print(new_track)
+            Spotify.user_tracks[str(after.id)] = new_track
+            await self.bot.get_channel(738607027022069840).send('user changed to new track')
+    """
 
 
 # The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case MembersCog.
