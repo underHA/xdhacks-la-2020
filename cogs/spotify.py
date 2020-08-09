4b9f3d302b6f5a54f1733657ae10332a0847fa2a
Voctor <voctorbidoof@gmail.com>
2020-08-09 00:20:36 -0700

    Added MongoDB capabilities, Soundness Streaks

--- cogs/spotify.py
+++ cogs/spotify.py
@@ -13,8 +13,11 @@ from pymongo import MongoClient
 
 # For environment variables
 import os
-
 cluster = MongoClient(os.environ.get('MONGO_KEY'))
+
+db = cluster["soundmood"]
+collection = db["stats"]
+optout = db["optout"]
 
 # Spotify
 
@@ -23,15 +26,13 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-badList = ["pop", "edm", "r&b", "rap", "trap", "hip hop", "metal", "punk", "rock"]
+badList = ["edm", "r&b", "rap", "trap", "hip hop", "metal", "punk"]
+goodList = ["jazz", "classical", "lo-fi", "chill"]
 
 # Sending announcements to other servers based on the on_ready event in main
 class Spotify(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    user_tracks = {}
-    queue = []
 
     @commands.group(name='spotify', aliases=['sptfy'])
     async def spotify(self, ctx):
@@ -62,13 +63,18 @@ class Spotify(commands.Cog):
     # Check if user's Spotify changed
     @commands.Cog.listener()
     async def on_member_update(self, before, after):
+
+        if optout.find_one({"id_":after.id}):
+            # If the user has opted out, don't go through
+            return
+
+
         old_track = '' 
         new_track = ''
 
-        track_activity = {}
-
-        if before.id not in Spotify.user_tracks:
-            Spotify.user_tracks[str(before.id)] = ''
+        # Check if user is already in database, add them if not
+        if not collection.find_one({"id_":after.id}):
+            collection.insert_one({"id_":after.id, "streak": 0, "track": ""})
 
         # Compare before
         for activity in before.activities:
@@ -79,16 +85,17 @@ class Spotify(commands.Cog):
         for activity in after.activities:
             if isinstance(activity, discord.Spotify):
                 new_track = activity.track_id
-                track_activity = activity
 
+
+        user = collection.find_one({"id_":after.id})
 
         # If the user changed Spotify tracks
-        if old_track != new_track and new_track != '' and new_track != Spotify.user_tracks[str(after.id)]:
+        if old_track != new_track and new_track != '' and new_track != user["track"]:
             # before in this context is used as Discord ID
   
-
             # If we got here, then the bot can detect if a user switches to a new song
-            Spotify.user_tracks[str(after.id)] = new_track
+            # Update track on mongoDB
+            collection.update_one({"id_": after.id}, {"$set": {"track": new_track}})
 
             sp_track = sp.track(activity.track_id)
             sp_artist = sp.artist(sp_track["artists"][0]["id"])
@@ -100,11 +107,60 @@ class Spotify(commands.Cog):
             await self.bot.get_channel(741774089232056472).send(f"{after} is now listening to **{activity.title}** by {activity.artist}.\nGenres: {', '.join(genres)}")
             
             if any(g in genre for genre in genres for g in badList):
-                await self.bot.get_user(id=int(after.id)).send("It looks like you are listening to a song that could decrease your productivity.")
+                # Check if they have a streak
+                streak_string = ''
+                if user["streak"] != 0:
+                    streak_string = f'\n😥 You lost your {user["streak"]} song Soundness Streak! Try playing a few songs beneficial your wellness to build it back up again.'
+
+                await self.bot.get_user(id=int(after.id)).send(f"It looks like you are listening to a song that could decrease your productivity.{streak_string}")
+
+                # Set user's wellness song streak to 0
+                collection.update_one({"id_": after.id}, {"$set": {"streak": 0}})
+            else:
+                # Else, increment their song streak
+                collection.update_one({"id_": after.id}, {"$inc": {"streak": 1}})
+
+                new_user = collection.find_one({"id_":after.id})
+                
+                # If their streak is a multiple of 5, congratulate them
+                if new_user["streak"] % 5 == 0:
+                    await self.bot.get_user(id=int(after.id)).send(f"You're on a {new_user['streak']} song Soundness Streak 🔥! Keep it up!")
         
+
+    @commands.command(name="optout")
+    async def optout(self, ctx):
+        optout.insert_one({"_id": ctx.author.id})
+        await ctx.send(f"You have **opted out** of A Sound Mood. To join the program again, use ?optin.")
+
+    @commands.command(name="optin")
+    async def optin(self, ctx):
+        optout.delete_one({"_id": ctx.author.id})
+        await ctx.send(f"You have **opted into** A Sound Mood. To leave the program, use ?optout.")
+
     @commands.command(name="activity", aliases=["recommend"])
     async def activity(self, ctx, activity:str):
         await ctx.send(f"hi, you want {activity}?")
+
+    @activity.error
+    async def activity_error(self, ctx, error):
+        await ctx.send('Please specify what activity you\'re working on!\nExample: ?activity homework')
+
+    @commands.command(name="stats", aliases=["stat", "score"])
+    async def stats(self, ctx, user: discord.Member=None):
+        user = user or ctx.author
+
+        if optout.find_one({"_id": user.id}) or not collection.find_one({"id_":user.id}):
+            await ctx.send(f"Sorry, that user has either opted out or is not in our system!")
+        else:
+            embed = discord.Embed(title=f"{user}'s Soundness Score",
+                color=random.randint(0, 0xFFFFFF))
+        
+            embed.set_thumbnail(url=user.avatar_url)
+            embed.add_field(name="Streak", value=collection.find_one({"id_":user.id})["streak"])
+
+            embed.set_footer(text=f"Requested by @{ctx.message.author}", icon_url=ctx.message.author.avatar_url)
+
+            await ctx.send('', embed=embed)
 
 
 # The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case MembersCog.
